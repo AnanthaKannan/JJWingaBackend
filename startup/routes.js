edaf2c2b18e7b89c49ed1dbe6584918ff46f1890
AnanthaKannan <sreeananthakannan@gmail.com>
2026-05-27 21:43:44 +0530

    fixed

--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const admin = require("../routes/admin");
-const error = require("./error")
-module.exports = function(app) {
-    app.use(express.json());
-    app.use("/api/admin", admin);
-    app.use(error)
-}
+const routes = require("../routes");
+const error = require("./error");
+module.exports = function (app) {
+  app.use(express.json());
+  app.use("/v1/api", routes);
+  app.use(error);
+};
